736bbeb1fea914a6158d1796742efbdfa8015280
Rajesh G.Astarkar <104189277+rajeshastarkar@users.noreply.github.com>
2022-04-25 13:58:56 +0530

    Update DemoGitHub.java

--- 20.1_GitHub/src/github/DemoGitHub.java
+++ 20.1_GitHub/src/github/DemoGitHub.java
@@ -2,10 +2,7 @@ package github;
 
 public class DemoGitHub
 {
-	public void m1()
-	{
-		System.out.println("m1--A");
-	}
+	
 	public void m2()
 	{
 		System.out.println("m2--A");
@@ -19,7 +16,7 @@ public class DemoGitHub
 		System.out.println("CJC JAVA");	
 		
 		DemoGitHub g = new DemoGitHub();
-		g.m1();
+		
 		g.m2();
 		g.m3();
 	}
