a273280aa770e123017d5d85b418c2c35c687b18
LENOVO <LENOVO@DESKTOP-BHLKCQ0>
2022-04-25 12:35:38 +0530

    CJC25

--- 20.1_GitHub/src/github/DemoGitHub.java
+++ 20.1_GitHub/src/github/DemoGitHub.java
@@ -6,12 +6,17 @@ public class DemoGitHub
 	{
 		System.out.println("m1--A");
 	}
+	public void m2()
+	{
+		System.out.println("m2--A");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("CJC JAVA");	
 		
 		DemoGitHub g = new DemoGitHub();
 		g.m1();
+		g.m2();
 	}
 	
 }
